4a9077edb72770362c3aa8957610a13d237a58e6
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-12-06 03:07:43 -0900

    sync

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const { username, botname, passowrd, wiki } = require('./config.json');
+const { username, botname, password, apiUrl } = require('./config.json');
 const got = require('got');
+const { mwn } = require('mwn');
 
 require('mx-color-logger').init();
+
+const run = async () => {
+    const bot = await mwn.init({
+        apiUrl,
+        username: username + '@' + botname,
+        password,
+        defaultParams: {
+            assert: 'user'
+        }
+    });
+}
