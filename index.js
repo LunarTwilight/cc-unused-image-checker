d16b13df104d073f4075b0785d11d9471e099fa9
Chemie~ <13520392+LunarTwilight@users.noreply.github.com>
2021-12-06 04:15:02 -0900

    sync

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const { username, botname, password, apiUrl } = require('./config.json');
 const got = require('got');
+const cron = require('node-cron');
 const { mwn } = require('mwn');
 
 require('mx-color-logger').init();
 
-const run = async () => {
+(async () => {
     const bot = await mwn.init({
         apiUrl,
         username: username + '@' + botname,
         password,
         defaultParams: {
             assert: 'user'
-        }
+        },
+        userAgent: 'cc-unused-image-checker'
     });
-}
+    //cron.schedule('0 0 * * *', () => {
+        const timestamp = new Date(Date.now() - (24 * 60 * 60 * 1000)).toISOString();
+        console.log(timestamp);
+        let files = [];
+        for await (let json of bot.continuedQueryGen({
+            action: 'query',
+            list: 'allimages',
+            aisort: 'timestamp',
+            aistart: timestamp,
+            ailimit: 'max'
+        })) {
+            let res = json.query.allimages.map(item => item.name);
+            files = files.concat(res);
+        }
+        if (!files.length) {
+            mwn.log('[I] No results, aborting run.');
+            return;
+        }
+        console.log(files);
+    //});
+})();
